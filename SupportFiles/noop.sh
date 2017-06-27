cff845126eeb82ce11b341ffd82459534afb7255
Thomas H Jones II <github@xanthia.com>
2017-06-27 16:24:09 -0400

    Dummy script to make travis happy (for now)

---
+++ SupportFiles/noop.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# Dummy script to keep Travis from declaring a push-failure.
+#
+# This will be removed once there are support-scripts to push.
+#################################################################
