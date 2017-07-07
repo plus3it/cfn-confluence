0d82aea367ed50393020084280bcf2d30e6331da
Thomas H Jones II <github@xanthia.com>
2017-07-07 18:16:05 -0400

    First-pass OS-prep script

--- SupportFiles/noop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# Dummy script to keep Travis from declaring a push-failure.
-#
-# This will be removed once there are support-scripts to push.
-#################################################################
