2b114a515111c9c8c5988c7b541ab59019bc5ca4
Thomas H Jones II <github@xanthia.com>
2017-07-11 17:09:47 -0400

    Install Confluence

---
+++ SupportFiles/confluence_appinstall.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+# shellcheck disable=
+#
+#################################################################
+# shellcheck disable=SC2086
+PROGNAME="$(basename ${0})"
+SELMODE="$(awk -F= '/^SELINUX=/{print $2}' /etc/selinux/config)"
+BINSTALL=/root/atlassian-confluence-installer_x64.bin
+RESPFILE=/root/response.varfile
+SHARESRVR="${CONFLUENCE_SHARE_SERVER:-UNDEF}"
+
+##
+## Set up an error logging and exit-state
+function err_exit {
+   local ERRSTR="${1}"
+   local SCRIPTEXIT=${2:-1}
+
+   # Our output channels
+   echo "${ERRSTR}" > /dev/stderr
+   logger -t "${PROGNAME}" -p kern.crit "${ERRSTR}"
+
+   # Need our exit to be an integer
+   if [[ ${SCRIPTEXIT} =~ ^[0-9]+$ ]]
+   then
+      exit "${SCRIPTEXIT}"
+   else
+      exit 1
+   fi
+}
+
+##
+## Create persistent content-dirs in shared fileservice
+function MkPersistDir {
+   local NEWDIR="${1}"
+
+   if [[ -d ${NEWDIR} ]]
+   then
+      err_exit "Found ${NEWDIR} where none should have existed"
+   else
+      install -d -m 755 /mnt/${NEWDIR} || \
+        err_exit "Failed to create ${SHARESRVR}:/${NEWDIR}"
+   fi
+}
+
+##
+## Mount persistent content-dirs
+function MtPersistDir {
+   local SRCDIR="${1}"
+   local DSTDIR="$(echo ${SRCDIR} | sed -e 's#_#/#' -e 's#^#/#')
+
+   # Verify source exists (fix as necessary)
+   if [[ ! -d /mnt:/${SRCDIR} ]]
+   then
+      err_exit "Aborting: ${SRCDIR} does not exist."
+   fi
+
+   # Verify destination exists (fix as necessary)
+   if [[ ! -d ${DSTDIR} ]]
+   then
+      install -d -m 755 "${DSTDIR}" ||
+        err_exit "Could not create ${DSTDIR}"
+   fi
+
+   printf "Attempting to mount %s... " "${MNTPNT}"
+   mount "${SHARESRVR}":/"${TSTMNT}" "${MNTPNT}" && echo "Success" ||
+     err_exit "Failed to mount ${MNTPNT}"
+}
+
+
+   done
+
+
+   grep "${SHARESRVR}" /proc/mounts >> /etc/fstab ||
+     err_exit 'Failed to update /etc/fstab'
+}
+
+##
+## Nuke the dummy data
+function CleanDummy {
+   local DUMMYDIR="$(echo ${1} | sed -e 's#_#/#' -e 's#^#/#')
+
+   printf "Attempting to clean %s... " "${DUMMYDIR}"
+   rm -rf "${DUMMYDIR}"/* 2> /dev/null && \
+     echo "Success" || \
+     err_exit "Failed to remove all files from ${DUMMYDIR}"
+}
+
+
+##
+## Main script logic
+
+# Make ready for unattended install
+cat > "${RESPFILE} << EOF
+executeLauncherAction$Boolean=true
+app.install.service$Boolean=true
+sys.confirmedUpdateInstallationString=false
+launch.application$Boolean=true
+existingInstallationDir=/opt/Confluence
+sys.languageId=en
+sys.installationDir=/opt/atlassian/confluence
+EOF
+
+# Prep storage for install
+mount "${SHARESRVR}":/ /mnt 2> /dev/null || \
+  err_exit 'Cannot mount shared filesystems server'
+
+if [ "$(find /mnt -name server.xml)" = "" ]
+then
+   echo "This is a fresh install"
+
+   # Create and mount persistent-content dirs
+   for DIR in opt_atlassian var_atlassian
+   do
+      MkPersistDir "${DIR}" 
+      MtPersistDir "${DIR}"
+   done
+
+   "${BINSTALL}" -q -varfile "${SHARESRVR}"
+else
+   echo "This is a rebuild"
+   "${BINSTALL}" -q -varfile "${SHARESRVR}"
+   service confluence stop
+   for DIR in opt_atlassian var_atlassian
+   do
+      CleanDummy "${DIR}"
+      MtPersistDir "${DIR}"
+   done
+fi
+
+umount "${SHARESRVR}":/ 2> /dev/null || \
+  err_exit 'Cannot umount shared filesystems-root'
+
+grep "${SHARESRVR}" /proc/mounts || \
+  err_exit 'Failed to update /etc/fstab'
