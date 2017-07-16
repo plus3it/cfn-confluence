6cd1ecbdc0041eb746e73fa8112253c2086988d7
Thomas H Jones II <ferricoxide@users.noreply.github.com>
2017-07-16 09:51:27 -0400

    Initial PR (#1)
    Create linked template-set:
    * Create (EFS-oriented) parent template content to drive:
      * Create security-groups templage
      * Create EFS-hosted storage for Confluence templage
      * Create the Confluence database (single AZ) templage
      * Create S3 backup-bucket for Confluence templage
      * Create Confluence's instance role(s) templage
      * Create EC2 child-template to parent templage
      * Create OS-prep script
      * Create app-install script

---
+++ SupportFiles/confluence_appinstall.sh
@@ -0,0 +1,181 @@
+#!/bin/bash
+# shellcheck disable=SC2115,SC2155,SC2015,SC2034
+#
+#################################################################
+# shellcheck disable=SC2086
+PROGNAME="$(basename ${0})"
+SELMODE="$(awk -F= '/^SELINUX=/{print $2}' /etc/selinux/config)"
+BINSTALL=/root/atlassian-confluence-installer_x64.bin
+RESPFILE=/root/response.varfile
+SHARESRVR="${CONFLUENCE_SHARE_SERVER:-UNDEF}"
+PROXYFQDN="${CONFLUENCE_PROXY_FQDN:-UNDEF}"
+SERVERXML="/opt/atlassian/confluence/conf/server.xml"
+
+##
+## Set up an error logging and exit-state
+function err_exit {
+   local ERRSTR="${1}"
+   local SCRIPTEXIT=${2:-1}
+
+   # Our output channels
+   echo "${ERRSTR}" > /dev/stderr
+   logger -t "${PROGNAME}" -p kern.crit "${ERRSTR}"
+
+   # Need our exit to be an integer
+   if [[ ${SCRIPTEXIT} =~ ^[0-9]+$ ]]
+   then
+      exit "${SCRIPTEXIT}"
+   else
+      exit 1
+   fi
+}
+
+##
+## Create persistent content-dirs in shared fileservice
+function MkPersistDir {
+   local NEWDIR="${1}"
+
+   if [[ -d ${NEWDIR} ]]
+   then
+      err_exit "Found ${NEWDIR} where none should have existed"
+   else
+      install -d -m 755 /mnt/${NEWDIR} || \
+        err_exit "Failed to create ${SHARESRVR}:/${NEWDIR}"
+   fi
+}
+
+##
+## Mount persistent content-dirs
+function MtPersistDir {
+   local SRCDIR="${1}"
+   # shellcheck disable=SC2155
+   local DSTDIR="$(echo ${SRCDIR} | sed -e 's#_#/#' -e 's#^#/#')"
+
+   # Verify source exists (fix as necessary)
+   if [[ ! -d /mnt/${SRCDIR} ]]
+   then
+      err_exit "Aborting: ${SRCDIR} does not exist."
+   fi
+
+   # Verify destination exists (fix as necessary)
+   if [[ ! -d ${DSTDIR} ]]
+   then
+      install -d -m 755 "${DSTDIR}" ||
+        err_exit "Could not create ${DSTDIR}"
+   fi
+
+   printf "Attempting to mount %s... " "${DSTDIR}"
+   mount "${SHARESRVR}":/"${SRCDIR}" "${DSTDIR}" && echo "Success" ||
+     err_exit "Failed to mount ${DSTDIR}"
+}
+
+##
+## Nuke the dummy data
+function CleanDummy {
+   local DUMMYDIR="$(echo ${1} | sed -e 's#_#/#' -e 's#^#/#')"
+
+   printf "Attempting to clean %s... " "${DUMMYDIR}"
+   rm -rf "${DUMMYDIR}"/* 2> /dev/null && \
+     echo "Success" || \
+     err_exit "Failed to remove all files from ${DUMMYDIR}"
+}
+
+
+##
+## Main script logic
+setenforce 0
+
+
+# Make ready for unattended install
+cat > "${RESPFILE}" << EOF
+executeLauncherAction\$Boolean=true
+app.install.service\$Boolean=true
+sys.confirmedUpdateInstallationString=false
+launch.application\$Boolean=true
+existingInstallationDir=/opt/Confluence
+sys.languageId=en
+sys.installationDir=/opt/atlassian/confluence
+EOF
+
+# Prep storage for install
+mount "${SHARESRVR}":/ /mnt 2> /dev/null || \
+  err_exit 'Cannot mount shared filesystems server'
+
+if [ "$(find /mnt -name server.xml)" = "" ]
+then
+   echo "This is a fresh install"
+
+   bash "${BINSTALL}" -q -varfile "${RESPFILE}" || \
+     err_exit 'Installer did not run to clean completion'
+
+   while [[ $( netstat -46ln | grep -q :8000 )$? -ne 0 ]]
+   do
+      echo "Waiting for Confluence to finish initial start-up..."
+      sleep 10
+   done
+
+   printf 'Attempting to stop Confluence for reconfiguration... '
+   service confluence stop && echo 'Success' || \
+     err_exit 'Failed to stop Confluence'
+
+   echo "Relocating to persistent storage..."
+   mv /opt/atlassian /mnt/opt_atlassian || \
+     err_exit 'Failed to re-home /opt/atlassian to persistent storage'
+   mv /var/atlassian /mnt/var_atlassian || \
+     err_exit 'Failed to re-home /var/atlassian to persistent storage'
+
+   # Create and mount persistent-content dirs
+   for DIR in opt_atlassian var_atlassian
+   do
+      MtPersistDir "${DIR}"
+   done
+
+   # Make sure Confluence doesn't complain about being behind an ELB
+   if [[ ${PROXY} = UNDEF ]]
+   then
+      echo "No proxy-host passed to install-wrapper."
+   elif [[ -e ${SERVERXML} ]]
+   then
+      printf 'Attempting to add proxy-host to server.xml... '
+      # shellcheck disable=SC1004
+      sed -i '/Connector port="8090"/a \
+                proxyName="'${PROXYFQDN}'" proxyPort="443" scheme="https"' "${SERVERXML}" &&
+        echo 'Success' || \
+        err_exit 'Failed to add proxy-host to server.xml'
+   else
+      err_exit "Unable to find ${SERVERXML} to massage"
+   fi
+
+   printf 'Attempting post-reconfiguration restart of Confluence... '
+   service confluence start && echo 'Success' || \
+     err_exit 'Failed to restart Confluence'
+
+else
+   echo "This is a rebuild"
+   bash "${BINSTALL}" -q -varfile "${RESPFILE}" || \
+     err_exit 'Installer did not run to clean completion'
+
+   while [[ $( netstat -46ln | grep -q :8000 )$? -ne 0 ]]
+   do
+      echo "Waiting for Confluence to finish initial start-up..."
+      sleep 10
+   done
+
+   service confluence stop || \
+     err_exit 'Failed to stop disposable Confluence install'
+   for DIR in opt_atlassian var_atlassian
+   do
+      CleanDummy "${DIR}"
+      MtPersistDir "${DIR}"
+   done
+   service confluence start || \
+     err_exit 'Failed to start re-deployed Confluence application'
+fi
+
+umount "${SHARESRVR}":/ 2> /dev/null || \
+  err_exit 'Cannot umount shared filesystems-root'
+
+grep "${SHARESRVR}" /proc/mounts >> /etc/fstab || \
+  err_exit 'Failed to update /etc/fstab'
+
+setenforce ${SELMODE}
---
+++ SupportFiles/confluence_osprep.sh
@@ -0,0 +1,188 @@
+#!/bin/bash
+# shellcheck disable=SC2015
+#
+# Script to prepare the OS for installation of the Confluence
+# web application.
+#
+#################################################################
+# shellcheck disable=SC2086
+PROGNAME="$(basename ${0})"
+SELMODE="$(awk -F= '/^SELINUX=/{print $2}' /etc/selinux/config)"
+SHARESRVR="${CONFLUENCE_SHARE_SERVER:-UNDEF}"
+SHARETYPE="${CONFLUENCE_SHARE_TYPE:-UNDEF}"
+BINURL="${CONFLUENCE_INSTALLBIN_URL:-UNDEF}"
+RPMDEPLST=(
+      postgresql
+      postgresql-jdbc
+      nfs-utils
+      nfs4-acl-tools
+   )
+
+##
+## Set up an error logging and exit-state
+function err_exit {
+   local ERRSTR="${1}"
+   local SCRIPTEXIT=${2:-1}
+
+   # Our output channels
+   echo "${ERRSTR}" > /dev/stderr
+   logger -t "${PROGNAME}" -p kern.crit "${ERRSTR}"
+
+   # Need our exit to be an integer
+   if [[ ${SCRIPTEXIT} =~ ^[0-9]+$ ]]
+   then
+      exit "${SCRIPTEXIT}"
+   else
+      exit 1
+   fi
+}
+
+##
+## Add RPM dependencies
+function AddonRpms {
+   local INSTRPMS=()
+
+   case "${SHARETYPE}" in
+      UNDEF)
+         err_exit 'Shareserver type not defined'
+         ;;
+      nfs)
+         RPMDEPLST+=(
+               nfs-utils
+               nfs4-acl-tools
+            )
+         ;;
+      gluster)
+         RPMDEPLST+=(
+               glusterfs
+               glusterfs-fuse
+               attr
+            )
+         ;;
+   esac
+
+   # Check if needed RPMs are missing
+   for RPM in "${RPMDEPLST[@]}"
+   do
+      printf "Checking for presence of %s... " "${RPM}"
+      if [[ $(rpm --quiet -q "$RPM")$? -eq 0 ]]
+      then
+         echo "Already installed."
+      else
+         echo "Selecting for install"
+         INSTRPMS+=("${RPM}")
+      fi
+   done
+
+   # Install any missing RPMs
+   if [[ ${#INSTRPMS[@]} -gt 0 ]]
+   then
+      echo "Will attempt to install the following RPMS: ${INSTRPMS[*]}"
+      yum install -y "${INSTRPMS[@]}" || \
+         err_exit "Install of RPM-dependencies experienced failures"
+   else
+      echo "No RPM-dependencies to satisfy"
+   fi
+}
+
+##
+## Enable services needed by NFS-client
+function NfsClientSetup {
+   local NFSSVCS=(
+               rpcbind
+               nfs-lock
+               nfs-idmap
+            )
+   for SVC in "${NFSSVCS[@]}"
+   do
+      printf "Enabling %s... " "${SVC}"
+      systemctl enable "${SVC}" && echo "Success" || \
+        err_exit "Failed to enable ${SVC}"
+
+      printf "Starting %s... " "${SVC}"
+      systemctl start "${SVC}" && echo "Success" || \
+        err_exit "Failed to start ${SVC}"
+   done
+}
+
+
+###########################
+## Main program contents ##
+###########################
+
+# Verify that critical parms were found
+if [[ ${SHARESRVR} = UNDEF ]] ||
+   [[ ${SHARETYPE} = UNDEF ]]
+   [[ ${BINURL} = UNDEF ]]
+then
+   err_exit 'A necessary parameter was not passed'
+fi
+
+# Install necessary extra RPMs
+AddonRpms
+
+##
+## Create firewalld service-definition for Confluence
+printf "Creating service-definition template for Confluence... "
+cat > /tmp/confluence.xml << EOF
+<?xml version="1.0" encoding="utf-8"?>
+<service>
+   <short>Confluence</short>
+   <description>Confluence documentation collaboration service</description>
+   <port protocol="tcp" port="8000"/>
+   <port protocol="tcp" port="8090"/>
+</service>
+EOF
+
+# shellcheck disable=SC2181
+if [[ $? -eq 0 ]]
+then
+   echo "Success"
+else
+   err_exit 'Failed to create the Confluence firewalld template'
+fi
+
+##
+## Configure firewall
+if [[ ${SELMODE} = enforcing ]]
+then
+   printf "Dialing back SEL mode... "
+   setenforce 0 || err_exit 'Failed to loosen SEL enforcement-mode'
+fi
+
+printf "Reading service-definition template for Confluence... "
+firewall-offline-cmd --new-service-from-file=/tmp/confluence.xml && \
+  echo "Success" || \
+  err_exit 'Failed reading firewalld service-definition for Confluence'
+
+printf "Restarting firewalld... "
+systemctl restart firewalld && \
+  echo "Success" || \
+  err_exit 'Failed restarting firewalld'
+
+echo "Getting firewalld info for Confluence"
+(
+  firewall-cmd --permanent --service=confluence --get-short
+  firewall-cmd --permanent --service=confluence --get-description
+  firewall-cmd --info-service=confluence
+  firewall-cmd --add-service=confluence
+  firewall-cmd --permanent --add-service=confluence
+  iptables -n -L IN_public_allow
+) | sed 's /^/  '
+
+setenforce "${SELMODE}" || err_exit "Failed setting SEL mode to ${SELMODE}"
+
+##
+## Enabling services
+NfsClientSetup
+
+# Stage the Confluence binary-installer
+printf "Fetching Confluence binary-installer... "
+curl -skL "${BINURL}" -o /root/atlassian-confluence-installer_x64.bin && \
+   echo "Success" || \
+   err_exit 'Failed to download the binary-installer'
+
+
+# Shared filesystems done in App-installer due to 
+# having to account for the specific needs of a
+# fresh vs. redeploy install.
